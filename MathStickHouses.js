2335ad668a2ab0b9646d14bf3c3202226b0b4f88
GalaSport <146236308+GalaSport@users.noreply.github.com>
2024-09-24 17:57:11 +0300

    Add files via upload

---
+++ MathStickHouses.js
@@ -0,0 +1,9 @@
+function mathStickHouses(step){
+    let result = (step * 6) - (step - 1);
+    console.log(result); 
+}
+mathStickHouses(1);
+mathStickHouses(2);
+mathStickHouses(3);
+mathStickHouses(4);
+mathStickHouses(87);
